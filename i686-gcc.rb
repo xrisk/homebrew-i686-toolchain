da9dbf6dbeb573997669b91e6e3c1ab498fffeb3
Rishav Kundu <rishav.kundu98@gmail.com>
2017-01-03 19:54:45 +0530

    Add gcc

---
+++ i686-gcc.rb
@@ -0,0 +1,45 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class I686Gcc < Formula
+  desc ""
+  homepage ""
+  url "ftp://ftp.gnu.org/gnu/gcc/gcc-6.3.0/gcc-6.3.0.tar.bz2"
+  sha256 "f06ae7f3f790fbf0f018f6d40e844451e6bc3b7bc96e128e63b09825c1f8b29f"
+
+  # depends_on "cmake" => :build
+
+  depends_on "gmp"
+  depends_on "libmpc"
+  depends_on "mpfr"
+  depends_on "isl"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--prefix=#{prefix}",
+                          "--target=i686-elf",
+                          "--disable-nls",
+                          "--enable-languages=c,c++",
+						  "--without-headers",
+    # system "cmake", ".", *std_cmake_args
+    system "make", "all-gcc"
+	system "make", "all-target-libgcc"
+	system "make", "install-gcc"
+	system "make", "install-target-libgcc"
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test i686-gcc`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "#{bin}/i686-elf-gcc"
+  end
+end
