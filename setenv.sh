66c4abe2c8a96e2a9e4957061847a040d5431399
Rishav Kundu <rishav.kundu98@gmail.com>
2017-01-05 20:52:48 +0530

    Fix arx

--- setenv.sh
+++ setenv.sh
@@ -4,4 +4,4 @@
 
 export CC="$(brew --prefix i686-elf-gcc)/bin/i686-elf-gcc"
 export LD="$(brew --prefix i686-elf-binutils)/bin/i686-elf-ld"
-export AR="$(brew --prefix i686-elf-binutils)/bin/i686-elf-binutils"
+export AR="$(brew --prefix i686-elf-binutils)/bin/i686-elf-ar"
