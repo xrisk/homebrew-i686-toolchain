1ef0f628734411b5237ae96515f25582754ad91a
Rishav Kundu <rishav.kundu98@gmail.com>
2017-01-05 20:41:16 +0530

    Create formula directory

---
+++ setenv.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Run `source ~/cross-dev.sh` when using the toolchain.
+
+export CC="$(brew --prefix i686-elf-gcc)/bin/i686-elf-gcc"
+export LD="$(brew --prefix i686-elf-binutils)/bin/i686-elf-ld"
+export AR="$(brew --prefix i686-elf-binutils)/bin/i686-elf-binutils"
